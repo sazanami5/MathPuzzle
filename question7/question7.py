3fea741486eed6ea059fb1b185578e0eb1e056f1
sazanami5 <hbws001@gmail.com>
2021-05-28 23:52:22 +0900

    fix:question7

--- question7/question7.py
+++ question7/question7.py
@@ -8,16 +8,13 @@ import datetime
 
 startDate = datetime.datetime(1964, 10, 10)
 endDate   = datetime.datetime(2020,  7, 24)
-endDate   = datetime.datetime(1964,  10, 15)
 beforeDate = startDate + datetime.timedelta(days = 1)
 
 while(True):
     beforeDate = beforeDate + datetime.timedelta(days = 1)
-    reversedDate = format(int(beforeDate.strftime("%Y%M%d")),"b")[::-1]
+    reversedDate = format(int(beforeDate.strftime("%Y%m%d")),"b")[::-1]
     restoredDate = int(reversedDate, 2)
-    print(restoredDate)
-    print(int(beforeDate.strftime("%Y%M%d")))
-    if restoredDate == int(beforeDate.strftime("%Y%M%d")):
+    if restoredDate == int(beforeDate.strftime("%Y%m%d")):
         print(beforeDate)
     
     if beforeDate == endDate:
