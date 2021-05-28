ecfb40d82c75093dbe9c78a0cf6c03b6152f4731
sazanami5 <hbws001@gmail.com>
2021-05-28 23:46:18 +0900

    feat:7

---
+++ question7/question7.py
@@ -0,0 +1,24 @@
+# 1964-10-10 ~ 2020-07-24
+# 二進数に変換
+# に進数を逆に並べる
+# 10進数に戻す
+#この時元の数字と同じ年を出力
+
+import datetime
+
+startDate = datetime.datetime(1964, 10, 10)
+endDate   = datetime.datetime(2020,  7, 24)
+endDate   = datetime.datetime(1964,  10, 15)
+beforeDate = startDate + datetime.timedelta(days = 1)
+
+while(True):
+    beforeDate = beforeDate + datetime.timedelta(days = 1)
+    reversedDate = format(int(beforeDate.strftime("%Y%M%d")),"b")[::-1]
+    restoredDate = int(reversedDate, 2)
+    print(restoredDate)
+    print(int(beforeDate.strftime("%Y%M%d")))
+    if restoredDate == int(beforeDate.strftime("%Y%M%d")):
+        print(beforeDate)
+    
+    if beforeDate == endDate:
+        break
